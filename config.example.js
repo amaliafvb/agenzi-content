5759eafe4283fe72787d5fe1e6d5844c18c4f2cf
amaliafvb <amaliafvb@gmail.com>
2026-09-22 20:59:28 +0700

    chore: add config example

---
+++ config.example.js
@@ -0,0 +1,1 @@
+window.AGENZI_CONFIG={SUPABASE_URL:"https://YOUR_PROJECT.supabase.co",SUPABASE_PUBLISHABLE_KEY:"sb_publishable_xxxxxxxxxxxxxxxxx",INTERNAL_EMAIL_DOMAIN:"internal.agenzi.local",APP_NAME:"AGENZI Studio",APP_SUBTITLE:"Internal Content OS"};
